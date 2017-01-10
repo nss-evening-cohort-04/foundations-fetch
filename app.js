49ebe0000f59e8437e35346d34d40612daba022a
jessica kilby <jessicakilby@gmail.com>
2017-01-09 22:43:53 -0600

    now equals 2 in dom

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-;(function(){
+//will work now, after ng-app directive added
 
-	app = { myApp: "yes"};
-	console.log("one");
+angular.module("MyApp", []);
 
-})();
 
+// ;(function(){
+
+// 	app = { myApp: "yes"};
+// 	console.log("one");
+
+// })();
+
+// ;(function(){
+
+// 	app = { myApp: "no"};
+// 	console.log("two");
+
+// })();
 //iife ability to name things the same name
 //make it more secure
 //won't be on the global scope
-
-;(function(){
-
-	app = { myApp: "no"};
-	console.log("two");
-
-})();
