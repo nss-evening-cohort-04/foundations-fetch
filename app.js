a32889604ba5a1673201e07107079666252df5b4
Bryan Hankins <bryanhankins@me.com>
2017-01-10 21:38:19 -0600

    add factory controller and service

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 ;(function(){
-	var app = angular.module('myApp', [])
+	const app = angular.module('fetchApp', []);
+
+	debugger
+
+	app.controller('MainController', function($scope, requestService, Response){
+		$scope.responses = [];
+		$scope.url='http://httpstat.us/200';
+		$scope.fetch = () => {
+			requestService.get($scope.url).then((success) => {
+				const response = new Response(success);
+				debugger
+				$scope.responses.push({statusCode: success.status});
+			},(error) => {
+				debugger
+			});
+		};
+	});
+
+	app.service('requestService', function($http){
+		this.get = (url) => $http.get(url);
+	});
+
+	app.factory('Response', function(){
+		const Response = function(ResponseData){
+			this.statusCode = ResponseData.status;
+		};
+		return Response;
+	});
 })();
