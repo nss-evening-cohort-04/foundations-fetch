812ede9acc42f86410d119be75c75478e91859b1
Alex Saber <Alex Saber>
2017-01-11 08:58:17 -0600

    move controller, factory, and service into one file for now

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,30 @@
-var myApp = angular.module("myApp", []);
+const myApp = angular.module("myApp", []);
+
+debugger
+
+myApp.controller("homeCtrl", function($scope, requestService, Response) {
+  $scope.responses = [];
+  $scope.url = "http://httpstat.us/200";
+
+  $scope.fetch = () => {
+    requestService.get($scope.url).then(success => {
+      const response = new Response(success);
+      $scope.responses.push(response);
+    }, (error) => {
+      debugger
+      //do something else
+    });
+  };
+});
+
+myApp.service("requestService", function($http) {
+  this.get = (url) => $http.get(url);
+});
+
+myApp.factory("Response", function() {
+  const Response = function (responseData) {
+    this.statusCode = responseData.status;
+  };
+
+  return Response;
+});
