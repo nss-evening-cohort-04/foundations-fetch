8e0cc7c0882dff9532faa5ed506e5dcb48fb8c9c
justin leggett <justin@codingrookie.com>
2017-01-11 16:24:57 -0600

    add dropdown

--- mainController.js
+++ mainController.js
@@ -1,17 +1,19 @@
-;(function () {
-  angular.module('fetchApp').controller('MainController', function ($scope, requestService, Response) {
-    $scope.responses = [];
-    $scope.url = "http://httpstat.us/200";
+;
+(function() {
+    angular.module('fetchApp').controller('MainController', function($scope, requestService, Response) {
+        $scope.responses = [];
+        $scope.url = "http://httpstat.us/200";
 
-    $scope.fetch = () => {
-      requestService.get($scope.url).then((success) => {
-        const response = new Response(success);
-        $scope.responses.push(response);
-      }, (error) => {
-        debugger
-        //do something else
-      });
-    };
-  });
+        $scope.methods = ['get', 'put', 'post', 'delete'];
+
+        $scope.fetch = () => {
+            requestService.get($scope.url).then((success) => {
+                const response = new Response(success);
+                $scope.responses.push(response);
+            }, (error) => {
+                debugger
+                //do something else
+            });
+        };
+    });
 })();
-
