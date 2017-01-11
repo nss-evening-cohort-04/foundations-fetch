182c50fce79365fe17ca8f7073837a0394b8ad7d
Debra Gordon <debra.m.gordon@gmail.com>
2017-01-10 20:57:51 -0600

    scaling back files

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,11 @@
 "use strict";
 
-var app = angular.module("myApp");
+var app = angular.module("fetchApp", ["ngRoute"]);
+
+app.controller("Default", function ($scope, requestService) {
+  $scope.message = 'hello';
+});
+
+app.service('requestService', function ($http) {
+  this.get = (url) => $http.get(url);
+});
