2e55e4b360079b2f997501b6f86f59d94e7176bc
Alicia Miceli <akb.miceli@gmail.com>
2017-01-10 21:35:24 -0600

    add a service and a controller

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-var app = angular.module("myApp", []);
+;(function(){
+	const app = angular.module("myApp", []);
+	
+
+	app.controller('MainController', function($scope, requestService, Response) {
+		$scope.responses = [];
+		$scope.url = "http://httpstat.us/200"
+
+		$scope.fetch = function() {};
+		requestService.get($scope.url).then(success => {
+			const response = new Response(success);
+			$scope.responses.push(response)
+		}, error => {
+			//do something else
+		})
+	});
+
+	app.service('requestService', function($http) {
+		this.get = (url) => $http.get(url);
+	});
+
+	app.factory('Response', function(){
+		const Response = function(responseData) {
+			this.statusCode = responseData.status;
+		};
+		return Response;
+	});
+})();
+
