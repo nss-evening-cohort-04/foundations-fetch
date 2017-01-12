6a4d2eb0182c7ae392d16754f5795ee47227203a
Alex Saber <Alex Saber>
2017-01-11 19:11:07 -0600

    move non-controller related code to separate, individual files; calculate http request response time

--- javascripts/controllers/homeCtrl.js
+++ javascripts/controllers/homeCtrl.js
@@ -1,9 +1,18 @@
-myApp.controller("homeCtrl", function($scope, requestService) {
-  $scope.fetch = () => {
-    requestService.get($scope.url)
-  };
-});
+(function() {
+  angular.module("myApp").controller("homeCtrl", function($scope, requestService, Response) {
+    $scope.responses = [];
+    $scope.url = "http://httpstat.us/200";
 
-myApp.service("requestService", function($http) {
-  this.get = (url) => $http.get(url);
-});
+    $scope.fetch = () => {
+      let timeRequestSent = Date.now();
+      requestService.get($scope.url).then(success => {
+        let timeRequestCompleted = Date.now();
+        let responseTime = timeRequestCompleted - timeRequestSent;
+        const response = new Response(success);
+        $scope.responses.push(response);
+      }, (error) => {
+        //do something else
+      });
+    };
+  });
+})();
