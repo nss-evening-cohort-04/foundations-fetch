3b5e860502ab4e0bc9a8506b440a345eec12e493
justin leggett <justin@codingrookie.com>
2017-01-11 18:59:40 -0600

    response_time is displaying correctly

--- mainController.js
+++ mainController.js
@@ -14,12 +14,13 @@
                     const response = new Response(success);
                     // push response to responses array
                     $scope.responses.push(response);
+                    response.response_time = new Date().getTime() - start_time;
                     // push response time to responses array
-                    let response_time = {
-                        response_time: new Date().getTime() - start_time
-                    };
+                    // let response_time = {
+                    //     response_time: new Date().getTime() - start_time
+                    // };
                     // add response_time to the responses array
-                    $scope.responses.push(response_time);
+                    // $scope.responses.push(response_time);
                     console.log("success = ", $scope.responses);
                 }, (error) => {
                     debugger
