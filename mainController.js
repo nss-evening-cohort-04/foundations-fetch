a33f51cb51279b77a84a3b38c166ecccc15e97c2
justin leggett <justin@codingrookie.com>
2017-01-12 09:03:46 -0600

    failed fetch request array displaying correctly

--- mainController.js
+++ mainController.js
@@ -2,6 +2,7 @@
 (function() {
     angular.module('fetchApp').controller('MainController', function($scope, requestService, Response) {
         $scope.responses = [];
+        $scope.failedresponses = [];
         $scope.url = "http://httpstat.us/200";
 
         $scope.methods = ['get', 'options', 'post', 'head'];
@@ -12,18 +13,20 @@
                 .then((success) => {
                     console.log(success);
                     const response = new Response(success);
-                    // push response to responses array
-                    $scope.responses.push(response);
                     // add the url to response
                     response.url = $scope.url;
                     // add the response_time to response
                     response.response_time = new Date().getTime() - start_time;
-                    console.log("$scope.responses = ", $scope.responses);
-                    console.log("success = ", success);
+                    // push response to responses array
+                    $scope.responses.push(response);
                 }, (error) => {
-                    console.log(error);
-                    debugger
-                    //populate another array when you receive an error
+                    const failedresponse = new Response(error);
+                    // add the url to response
+                    failedresponse.url = $scope.url;
+                    // add the response_time to response
+                    failedresponse.response_time = new Date().getTime() - start_time;
+                    // push response to responses array
+                    $scope.failedresponses.push(failedresponse);
                 });
         };
     });
