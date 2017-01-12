8d64c0689173be687bd8f45814b68076317a5351
Alex Saber <Alex Saber>
2017-01-11 19:10:02 -0600

    separate code out of app.js to individual files

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,30 +1,3 @@
-const myApp = angular.module("myApp", []);
-
-debugger
-
-myApp.controller("homeCtrl", function($scope, requestService, Response) {
-  $scope.responses = [];
-  $scope.url = "http://httpstat.us/200";
-
-  $scope.fetch = () => {
-    requestService.get($scope.url).then(success => {
-      const response = new Response(success);
-      $scope.responses.push(response);
-    }, (error) => {
-      debugger
-      //do something else
-    });
-  };
-});
-
-myApp.service("requestService", function($http) {
-  this.get = (url) => $http.get(url);
-});
-
-myApp.factory("Response", function() {
-  const Response = function (responseData) {
-    this.statusCode = responseData.status;
-  };
-
-  return Response;
-});
+(function() {
+  angular.module("myApp", []);
+})();
