3a5a9ad5c1f52f1939435990237749c3043e4f88
Nathan Ostrander <ostrander_nathan@yahoo.com>
2017-01-11 18:02:05 -0600

    add controller service factory

---
+++ mainController.js
@@ -0,0 +1,18 @@
+;(function () {
+
+  angular.module('fetchApp').controller('MainController', function ($scope, requestService, Response) {
+		$scope.responses = [];
+		$scope.url = "http://httpstat.us/200";
+
+	$scope.fetch = ()=>{
+			requestService.get($scope.url).then((success) =>{
+			const response = new Response(success);
+				$scope.responses.push(response);
+				// debugger
+			}, (error) =>{
+				debugger
+				// do something else
+			});
+		};
+	});
+})();
