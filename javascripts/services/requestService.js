fcd6432e26df648af31aac181649056c00a067e5
Alex Saber <Alex Saber>
2017-01-13 18:45:19 -0600

    add longhand options http request

--- javascripts/services/requestService.js
+++ javascripts/services/requestService.js
@@ -2,6 +2,10 @@
   angular.module("myApp").service("requestService", function($http) {
     this.get = (url) => $http.get(url);
     this.post = (url) => $http.post(url);
+    this.options = (url) => $http({
+      method: 'OPTIONS',
+      url: url
+    });
     this.head = (url) => $http.head(url);
   });
 })();
