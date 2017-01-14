6b1af9be328a89641df54c01a00d618395ea0bbf
jessica kilby <jessicakilby@gmail.com>
2017-01-13 22:28:59 -0600

    select working but doesn't display right

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,21 +1,48 @@
-;(function(){
-	angular.module("fetchApp").controller("MainController", function ($scope, requestService, Response){
-		$scope.responses = [];
-		$scope.url = "http://httpstat.us/200";
-		$scope.logTime = [];
+// ;(function(){
+// 	angular.module("fetchApp").controller("MainController", function ($scope, requestService, Response, $http){
 
-		$scope.fetch = () => {
-			const startTime = new Date().getTime();
-			requestService.get($scope.url).then((success) => {
-				const response = new Response(success);
-				const endTime = new Date().getTime();
-				const time = ((endTime - startTime)/1000);
-				$scope.responses.push(response);
-				$scope.logTime.push(time);
-			}, (error) => {
-				// debugger
-				//something else
-			})
-		};
-	});
-})();
+// 		$scope.url = "http://httpstat.us/200"; //just so you don't have to keep typing it
+// 		$scope.responses = [];
+// 		$scope.logTime = [];
+
+// 		$scope.fetch = () => {
+// 			const startTime = new Date().getTime();
+// 			$http({method: $scope.method}).then((success) => {
+// 				const response = new Response(success);
+// 				const endTime = new Date().getTime();
+// 				const time = ((endTime - startTime)/1000);
+// 				$scope.responses.push(response);
+// 				$scope.logTime.push(time);
+// 			}, (error) => {
+// 				console.log("an error occured");
+// 			});
+// 		};
+// 		});
+// })();
+
+(function(angular) {
+  'use strict';
+angular.module('fetchApp', []).controller('MainController', ['$scope', '$http', '$templateCache',
+    function($scope, $http, $templateCache) {
+      $scope.method = 'GET';
+      $scope.url = 'http://httpstat.us/200';
+
+      $scope.fetch = function() {
+        $scope.code = null;
+        $scope.responses = [];
+
+        $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
+          then(function(success) {
+          	const response = new Response(success);
+            $scope.statusCode = response.status;
+            $scope.data = response.data;
+            $scope.responses.push(response);
+          }, function(error) {
+            $scope.data = error.data || 'Request failed';
+            $scope.status = error.status;
+        });
+      };
+
+    
+    }]);
+})(window.angular)
--- requestService.js
+++ requestService.js
@@ -1,13 +1,10 @@
 ;(function () {
 
 	angular.module("fetchApp").service("requestService", function ($http){
-		this.get = (url) => $http.get(url);
-		this.post = (url) => $http.post(url);
-		this.head = (url) => $http.head(url);
-		this.options = (url) => $http ({
-			method: 'OPTIONS',
-			url: url
-		});
+		this.method = (url) => $http.method(url);
+		// this.post = (url) => $http.post(url);
+		// this.head = (url) => $http.head(url);
+		// this.options = (url) => $http.options(url);
 	});
 
 })();
