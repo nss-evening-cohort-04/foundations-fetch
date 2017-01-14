58d486bed68769ef17c9cabcd7671b953abc1261
jessica kilby <jessicakilby@gmail.com>
2017-01-13 20:40:38 -0600

    changed services js file

--- requestService.js
+++ requestService.js
@@ -2,19 +2,12 @@
 
 	angular.module("fetchApp").service("requestService", function ($http){
 		this.get = (url) => $http.get(url);
+		this.post = (url) => $http.post(url);
+		this.head = (url) => $http.head(url);
+		this.options = (url) => $http ({
+			method: 'OPTIONS',
+			url: url
+		});
 	});
-
-	// angular.module("fetchApp").service("requestService", function ($http){
-	// 	this.post = (url) => $http.post(url);
-	// });
-
-	// angular.module("fetchApp").service("requestService", function ($http){
-	// 	this.options = (url) => $http.options(url);
-	// });
-
-	// angular.module("fetchApp").service("requestService", function ($http){
-	// 	this.head = (url) => $http.head(url);
-	// });
-
 
 })();
